ca5ed9c31097c34e81a7e0d5943dc86bc9bd76c6
NadiaVoloshyna <dnipronadiya@gmail.com>
2020-12-22 13:13:07 +0200

    added maxLength to post title

--- src/modules/posts/dto/post.dto.ts
+++ src/modules/posts/dto/post.dto.ts
@@ -1,8 +1,9 @@
-import { IsNotEmpty, MinLength } from 'class-validator';
+import { IsNotEmpty, MinLength, MaxLength } from 'class-validator';
 
 export class PostDto {
     @IsNotEmpty()
     @MinLength(4)
+    @MaxLength(20)
     title: string;
 
     @IsNotEmpty()
